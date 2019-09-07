a829daae43c24728e281eb0b5052d34dafbdec6f
Thomas Pope <tpope0928@gmail.com>
2019-09-07 20:51:09 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
     def create
-        @user = User.find_or_create_by(uid: auth[:uid]) do |u| 
+        @user = User.find_or_create_by(uid: auth[:uid]) do |u|
             u.name = auth['info']['name']
         end
 
